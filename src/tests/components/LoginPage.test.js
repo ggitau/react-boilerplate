2b69544a8a7640763171a2046b160d642d528a7b
geogit <com00204@gmail.com>
2018-01-28 12:26:49 +0300

    First commit

---
+++ src/tests/components/LoginPage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {LoginPage} from '../../components/LoginPage';
+import { shallow } from 'enzyme'; 
+
+let wrapper, startLogin;
+
+beforeEach(() => {
+    startLogin = jest.fn();
+    wrapper = shallow(<LoginPage startLogin={startLogin}/>);
+});
+test('should correctly render LoginPage', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled();
+});
